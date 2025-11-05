32aaa2c83bdfa508df48cd8038c09672f467ba3b
Ahmed Shehta <63676946+ashehta700@users.noreply.github.com>
2025-11-05 13:05:20 +0300

    Delete microservices directory

--- microservices/services/products/app/utils.py
+++
@@ -1,6 +0,0 @@
-def success_response(message: str, data=None):
-    return {"success": True, "message": message, "data": data}
-
-def error_response(message: str, code: str = "ERROR"):
-    return {"success": False, "message": message, "error_code": code}
-
